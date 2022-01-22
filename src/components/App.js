76ab78f9f767875bc3f6b7a3fa963fdb331e1a51
chanigl <cg8552809@gmail.com>
2022-01-23 01:43:23 +0200

    מעודכן

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,30 @@ import {Number, arrNbr} from "./Number";
 
 function App() {
   return (
-    <div>
+    <div id="aa">
       {arrNbr.map((el,i)=>
       <Number
       key={i}
       nbr={el}
+      color={test(el)}
       />
       )}
     </div>
   );
+}
+function test(nbr){
+  if(nbr==2 || nbr==3 || nbr==5 || nbr==7){
+    return "red"
+  }
+  else if(nbr%2==0){
+    return "green"
+  }
+  else if(nbr%3==0 || nbr%5==0 ||nbr%7==0 ||nbr==1){
+    return "yellow"
+  }
+  else{
+    return "red"
+  }
 }
 
 export default App;
